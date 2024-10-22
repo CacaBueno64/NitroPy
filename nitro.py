3fb841f6da80126627e9697a95635159b871b63d
Isma <122747777+CacaBueno64@users.noreply.github.com>
2024-10-22 23:31:02 +0200

    Rewrite nitro.py

--- nitro.py
+++ nitro.py
@@ -1,101 +1,158 @@
+from struct import unpack, unpack_from, calcsize, Struct
 from io import BytesIO
-from struct import pack, unpack
+from enum import Enum
+import numpy as np
+from collections import OrderedDict
+
+
+def ReadSignature(reader, expectedSignature: int):
+    Signature = unpack("<I", reader.read(4))[0]
+    if Signature != expectedSignature:
+        raise Exception(f"Signature not correct.\nGot : {Signature}\nExpected : {expectedSignature}")
 
 class G3dFileHeader:
-    def __init__(self, data=None, expectedSignature=None, signature=None, version=None):
-        if data and expectedSignature:
-            self.Read(data, expectedSignature)
-        elif signature and version:
-            self.Signature  = signature
-            self.ByteOrder  = 0xFEFF
-            self.Version    = version
-            self.HeaderSize = 16
-
-    def Read(self, data, expectedSignature):
-        self.Signature = unpack("<I", data.read(4))[0]
-        self.ByteOrder = unpack("<H", data.read(2))[0]
-        self.Version = unpack("<H", data.read(2))[0]
-        self.FileSize = unpack("<I", data.read(4))[0]
-        self.HeaderSize = unpack("<H", data.read(2))[0]
-        self.NrBlocks = unpack("<H", data.read(2))[0]
-        self.BlockOffsets = []
-        
+    def __init__(self, reader=None, writer=None, version: int=None, signature: int=None, expectedSignature: int=None):
+        self.Header = Struct("<I HH I HH")
+        self.Signature = signature
+        self.ByteOrder = 0xFEFF
+        self.Version = version
+        self.HeaderSize = 16
+        if reader:
+            self.Read(reader, expectedSignature)
+    
+    def Read(self, reader, expectedSignature):
+        unpackedHeader = self.Header.unpack_from(reader.read(self.Header.size))
+        self.Signature = unpackedHeader[0]
+        self.Version = unpackedHeader[2]
+        self.NrBlocks = unpackedHeader[5]
         if self.Signature != expectedSignature:
-            raise ValueError(f"Wrong file signature\nGot : {self.Signature}\nExpected {expectedSignature}")
-        
-        for i in range(self.NrBlocks):
-            self.BlockOffsets.append(unpack("<I", data.read(4))[0])
+            raise Exception(f"Signature not correct.\nGot : {unpackedHeader[0]}\nExpected : {self.Signature}")
+        self.BlockOffsets = unpack(f"{self.NrBlocks}I", reader.read(self.NrBlocks*4))
 
-# not really a dictionary
 class G3dDictionary:
-    NAME_TOO_LONG_EXCEPTION_MESSAGE = "Names can be at most 16 characters long."
-    NAME_LENGTH = 16
+    def __init__(self, reader=None, writer=None, TData=None):
+        self.NAME_LENGTH = 16
+        self.Data = []
+        if reader:
+            self.Read(reader, TData)
     
-    UNSUPPORTED_REVISION_EXCEPTION_MESSAGE = "Unsupported dictionary revision. Got {0}, expected {1}."
-    ENTRY_SIZE_MISMATCH_EXCEPTION_MESSAGE = "Dictionary entry size mismatch. Got {0}, expected {1}."
-    INVALID_NUMBER_OF_BYTES_READ_EXCEPTION_MESSAGE = "Invalid number of bytes read for dictionary entry. {0} bytes were read, expected {1} bytes."
+    def Read(self, reader, TData):
+        G3dDictionarySerializer.ReadG3dDictionary(reader, TData, self)
     
+    def Add(self, name, data):
+        self.Data.append(G3dDictionaryEntry(name, data))
+    
+    def Count(self):
+        return len(self.Data)
+
+class G3dDictionaryEntry:
+    def __init__(self, name, data):
+        self.Name = name
+        self.Data = data
+
+class G3dDictionarySerializer:
     DICTIONARY_REVISION = 0
     NAME_LENGTH = 16
     
+    @staticmethod
+    def ReadG3dDictionary(reader, TData, dictionary):
+        startPosition = reader.tell()
+        # Header
+        revision = unpack("<B", reader.read(1))[0]
+        entryCount = unpack("<B", reader.read(1))[0]
+        dictionarySize = unpack("<H", reader.read(2))[0]
+        reader.read(2)
+        entriesOffset = unpack("<H", reader.read(2))[0]
+        G3dDictionarySerializer.ThrowIfInvalidRevision(revision)
+        # Entries
+        reader.seek(startPosition + entriesOffset)
+        entrySize = unpack("<H", reader.read(2))[0]
+        namesOffset = unpack("<H", reader.read(2))[0]
+        G3dDictionarySerializer.ThrowIfIncorrectEntrySize(entrySize, TData)
+        data = [TData(reader) for i in range(entryCount)]
+        reader.seek(startPosition + entriesOffset + namesOffset)
+        for i in range(entryCount):
+            name = reader.read(G3dDictionarySerializer.NAME_LENGTH).decode("shift-jis")
+            dictionary.Add(name, data[i])
+        reader.seek(startPosition + dictionarySize)
+    
+    @staticmethod
+    def ThrowIfInvalidRevision(revision):
+        if revision != G3dDictionarySerializer.DICTIONARY_REVISION:
+            raise Exception(f"Unsupported dictionary revision.\nGot {revision}\nExpected {G3dDictionarySerializer.DICTIONARY_REVISION}")
+    
+    @staticmethod
+    def ThrowIfIncorrectEntrySize(entrySize, TData):
+        if entrySize != TData.DataSize:
+            raise Exception(f"Dictionary entry size mismatch.\nGot {entrySize}, expected {TData.DataSize}.")
+
+class OffsetDictionaryData:
     DataSize = 4
     
-    def __init__(self, data=None):
-        self.dictionary = []
-        if data:
-            self.ReadG3dDictionary(data)
+    def __init__(self, reader=None, writer=None):
+        self.Offset = None
+        if reader:
+            self.Read(reader)
     
-    def ReadG3dDictionary(self, data):
-        startPosition = data.tell()
-        
-        # Header
-        revision = unpack("<B", data.read(1))[0]
-        entryCount = unpack("<B", data.read(1))[0]
-        dictionarySize = unpack("<H", data.read(2))[0]
-        data.read(2)
-        entriesOffset = unpack("<H", data.read(2))[0]
-        self.ThrowIfInvalidRevision(revision)
-        
-        # Entries
-        data.seek(startPosition + entriesOffset)
-        entrySize = unpack("<H", data.read(2))[0]
-        namesOffset = unpack("<H", data.read(2))[0]
-        Data = self.ReadData(data, entryCount)
-        
-        data.seek(startPosition + entriesOffset + namesOffset)
-        
-        for i in range(entryCount):
-            name = data.read(self.NAME_LENGTH).decode().strip("\x00")
-            self.dictionary.append([name, Data[i]])
-        
-        data.seek(startPosition + dictionarySize)
-    
-    def ThrowIfInvalidRevision(self, revision):
-        if revision != self.DICTIONARY_REVISION:
-            raise ValueError(f"{self.UNSUPPORTED_REVISION_EXCEPTION_MESSAGE}")
-    
-    def ReadData(self, data, entryCount):
-        Data = [None] * entryCount
-        for i in range(entryCount):
-            entryStart = data.tell()
-            Data[i] = unpack("<I", data.read(4))[0]
-            
-            dataRead = data.tell() - entryStart
-            if dataRead != self.DataSize:
-                raise ValueError(f"{self.INVALID_NUMBER_OF_BYTES_READ_EXCEPTION_MESSAGE}")
-        return Data
+    def Read(self, reader):
+        self.Offset = unpack("<I", reader.read(4))[0]
 
-def ReadSignature(data, expectedSignature):
-    signature = unpack("<I", data.read(4))[0]
-    if expectedSignature != signature:
-        raise ValueError(f"Wrong signature\nGot : {signature}\nExpected {expectedSignature}")
-    return signature
+class TextureToMaterialDictionaryData:
+    DataSize = 4
+    
+    def __init__(self, reader=None, writer=None):
+        self.Offset = None
+        self.Materials = []
+        if reader:
+            self.Read(reader)
+    
+    def Read(self, reader):
+        flags = unpack("<I", reader.read(4))[0]
+        self.Offset = flags & 0xFFFF
+        self.MaterialCount = flags >> 16 & 0x7F
+        self.Bound = flags >> 24 & 0xFF
 
-def Fx32(data):
-    return unpack("<i", data.read(4))[0] / 4096.0
-def Fx16(data):
-    return unpack("<h", data.read(2))[0] / 4096.0
-def ReadVecFx32(data):
-    return tuple(Fx32(data), Fx32(data), Fx32(data))
-def ReadVecFx16(data):
-    return tuple(Fx16(data), Fx16(data), Fx16(data))
+class PaletteToMaterialDictionaryData:
+    DataSize = 4
+    
+    def __init__(self, reader=None, writer=None):
+        self.Offset = None
+        self.Materials = []
+        if reader:
+            self.Read(reader)
+    
+    def Read(self, reader):
+        flags = unpack("<I", reader.read(4))[0]
+        self.Offset = flags & 0xFFFF
+        self.MaterialCount = flags >> 16 & 0x7F
+        self.Bound = flags >> 24 & 0xFF
+
+
+class GxPolygonAttr:
+    def __init__(self, value):
+        self._value = value
+class GxTexImageParam:
+    def __init__(self, value):
+        self._value = value
+
+class GxEnum(Enum):
+    pass
+
+
+def ReadFx16(reader):
+    return unpack("<H", reader.read(2))[0] / 4096
+def ReadFx16s(reader, count):
+    return [unpack("<H", reader.read(2))[0] / 4096 for i in range(count)]
+def ReadFx32(reader):
+    return unpack("<I", reader.read(4))[0] / 4096
+def ReadFx32s(reader, count):
+    return [unpack("<I", reader.read(4))[0] / 4096 for i in range(count)]
+def ReadVecFx16(reader):
+    return tuple([ReadFx16(reader) for i in range(3)])
+def ReadVecFx32(reader):
+    return tuple([ReadFx32(reader) for i in range(3)])
+
+
+class GLDisplayListBuffer:
+    def __init__(self, dl):
+        _vertexArray = GLVertexArray()
